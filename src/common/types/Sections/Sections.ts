4f37b66933678bb803269ae7ea6b7feeff11a16f
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-01-27 16:23:02 +0200

    Created section array mockup

---
+++ src/common/types/Sections/Sections.ts
@@ -0,0 +1,25 @@
+export const SECTIONS = [
+  {
+    id: "tab-1",
+    title: "Part 1: JavaScript language",
+    intro: "An Introduction",
+    links: [
+      { href: "/intro", label: "An Introduction to JavaScript" },
+      { href: "/", label: "Manuals and specifications" },
+      { href: "/", label: "Code editors" },
+      { href: "/", label: "Developer console" },
+    ],
+  },
+  {
+    id: "tab-2",
+    title: "Part 2: Browser: Document, Events, Interfaces",
+    intro: "JavaScript Fundamentals",
+    links: [
+      { href: "/", label: "Hello, World!" },
+      { href: "/", label: "Code structure" },
+      { href: "/", label: "Strict mode" },
+      { href: "/", label: "Variables" },
+      { href: "/", label: "Data types" },
+    ],
+  },
+];
