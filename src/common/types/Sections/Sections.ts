87d15be8a11c879301ff5405fef424b6a0a72e20
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-02-19 08:46:48 +0200

    Created template of sections

--- src/common/types/Sections/Sections.ts
+++ src/common/types/Sections/Sections.ts
@@ -1,25 +1,203 @@
 export const SECTIONS = [
   {
-    id: "tab-1",
-    title: "Part 1: JavaScript language",
-    intro: "An Introduction",
-    links: [
-      { href: "/intro", label: "An Introduction to JavaScript" },
-      { href: "/", label: "Manuals and specifications" },
-      { href: "/", label: "Code editors" },
-      { href: "/", label: "Developer console" },
+    id: "section-1",
+    intro: "Part 1: JavaScript language",
+    tabs: [
+      {
+        id: "tab-1",
+        intro: "An Introduction",
+        links: [
+          { href: "#intro", label: "An Introduction to JavaScript" },
+          {
+            href: "#manuals-specifications",
+            label: "Manuals and specifications",
+          },
+          { href: "#code-editors", label: "Code editors" },
+          { href: "#devtools", label: "Developer console" },
+        ],
+      },
+      {
+        id: "tab-2",
+        intro: "JavaScript Fundamentals",
+        links: [
+          { href: "#hello-world", label: "Hello, World!" },
+          { href: "structure", label: "Code structure" },
+          { href: "#strict-mode", label: "Strict mode" },
+          { href: "#variables", label: "Variables" },
+          { href: "#types", label: "Data types" },
+          {
+            href: "#alert-prompt-confirm",
+            label: "Interaction: alert, prompt, confirm",
+          },
+          { href: "#type-conversions", label: "Type conversions" },
+          { href: "#operators", label: "Basic operators, maths" },
+          { href: "#comparison", label: "Comparisons" },
+          { href: "#ifelse", label: "Conditional operators: if, '?' " },
+          { href: "#logical-operators", label: "Logical operators" },
+          {
+            href: "#nullish-coalescing-operator",
+            label: "Nullish coalescing operator '??' ",
+          },
+          { href: "#while-for", label: "Loops: while and for" },
+          { href: "#swithc", label: "The 'switch' statement" },
+          { href: "#function-basics", label: "Functions" },
+          { href: "#function-expressions", label: "Function expressions" },
+          {
+            href: "#arrow-functions-basics",
+            label: "Arrow functions, the basics",
+          },
+          { href: "#javascript-specials", label: "JavaScript specials" },
+        ],
+      },
+      {
+        id: "tab-3",
+        intro: "Code quality",
+        links: [
+          { href: "#debugging", label: "Debugging in Chrome" },
+          { href: "#coding-style", label: "Coding style" },
+          { href: "#comments", label: "Comments" },
+          { href: "#ninja-code", label: "Ninja code" },
+          { href: "#testing-mocha", label: "Automated testing with Mocha" },
+          { href: "#polyfills", label: "Polyfills and transpilers" },
+        ],
+      },
+      {
+        id: "tab-4",
+        intro: "Object: The Basics",
+        links: [],
+      },
+      {
+        id: "tab-5",
+        intro: "Data types",
+        links: [],
+      },
+      {
+        id: "tab-6",
+        intro: "Advanced working with functions",
+        links: [],
+      },
+      {
+        id: "tab-7",
+        intro: "Object properties configuration",
+        links: [],
+      },
+      {
+        id: "tab-8",
+        intro: "Prototypes, inheritance",
+        links: [],
+      },
+      {
+        id: "tab-9",
+        intro: "Classes",
+        links: [],
+      },
+      {
+        id: "tab-10",
+        intro: "Error handling",
+        links: [],
+      },
+      {
+        id: "tab-11",
+        intro: "Promises, async/await",
+        links: [],
+      },
+      {
+        id: "tab-12",
+        intro: "Generators, advanced iteration",
+        links: [],
+      },
+      {
+        id: "tab-13",
+        intro: "Modules",
+        links: [],
+      },
+      {
+        id: "tab-14",
+        intro: "Miscellaneous",
+        links: [],
+      },
     ],
   },
   {
-    id: "tab-2",
-    title: "Part 2: Browser: Document, Events, Interfaces",
-    intro: "JavaScript Fundamentals",
-    links: [
-      { href: "/", label: "Hello, World!" },
-      { href: "/", label: "Code structure" },
-      { href: "/", label: "Strict mode" },
-      { href: "/", label: "Variables" },
-      { href: "/", label: "Data types" },
+    id: "section-2",
+    intro: "Part 2: Browser: Document, Events, Interfaces",
+    tabs: [
+      {
+        id: "tab-1",
+        intro: "Document",
+        links: [],
+      },
+      {
+        id: "tab-2",
+        intro: "Introduction into Events",
+        links: [],
+      },
+      {
+        id: "tab-3",
+        intro: "UI Events",
+        links: [],
+      },
+      {
+        id: "tab-4",
+        intro: "Forms, controls",
+        links: [],
+      },
+      {
+        id: "tab-5",
+        intro: "Document and resource loading",
+        links: [],
+      },
+      {
+        id: "tab-6",
+        intro: "Additional articles",
+        links: [],
+      },
+      {
+        id: "miscellaneous",
+        intro: "Miscellaneous",
+        links: [],
+      },
+    ],
+  },
+  {
+    id: "section-3",
+    intro: "Part 3: Additional articles",
+    tabs: [
+      {
+        id: "tab-1",
+        intro: "Frames and windows",
+        links: [],
+      },
+      {
+        id: "tab-2",
+        intro: "Binary data, files",
+        links: [],
+      },
+      {
+        id: "tab-3",
+        intro: "Network requests",
+        links: [],
+      },
+      {
+        id: "tab-4",
+        intro: "Storing data in the browser",
+        links: [],
+      },
+      {
+        id: "tab-5",
+        intro: "Animation",
+        links: [],
+      },
+      {
+        id: "tab-6",
+        intro: "Web components",
+        links: [],
+      },
+      {
+        id: "tab-7",
+        intro: "Regular expressions",
+        links: [],
+      },
     ],
   },
 ];
