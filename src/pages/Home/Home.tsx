b649e04538ab40a644f2b9d533d77a0a5fa90f39
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-03-10 17:22:10 +0200

    Added isValidHash method

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,17 @@
 import styled from "@/DefaultTheme";
 import { Typography } from "@mui/material";
+import { useLocation } from "react-router-dom";
 import { TopicsList } from "@shared/TopicsList";
+import { SECTIONS } from "@/common/types";
+import { Pages } from "@/common/types/Pages/pages";
 
 export const Home = () => {
+  const location = useLocation();
+
+  const isValidHash = SECTIONS.flatMap((section) =>
+    section.tabs.flatMap((tab) => tab.links),
+  ).some((link) => link.href === location.hash);
+
   return (
     <Root>
       <HomeMenu>
@@ -11,20 +20,25 @@ export const Home = () => {
         <SubContent>
           <Typography variant="subtitle1">
             The modern JavaScript course for everyone! Master JavaScript with
-            projects, challenges and theory. Many courses in one!
+            projects, challenges, and theory. Many courses in one!
           </Typography>
         </SubContent>
       </HomeMenu>
-      <TopicsList />
+
+      {isValidHash && Pages[location.hash] ? (
+        Pages[location.hash]()
+      ) : (
+        <TopicsList />
+      )}
     </Root>
   );
 };
 
-const Root = styled("div")(({ theme }) => ({
+const Root = styled("div")({
   display: "flex",
   flexDirection: "column",
   height: "100%",
-}));
+});
 
 const HomeMenu = styled("div")(({ theme }) => ({
   position: "relative",
