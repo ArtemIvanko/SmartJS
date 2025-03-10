69e7e9602e8b19bfe428b2e0cf3b0e22adb840fc
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-03-10 18:59:39 +0200

    Created reusable StyledGlobalPage.tsx

---
+++ src/components/utils/StyledComponents/StyledGlobalPage.tsx
@@ -0,0 +1,9 @@
+import styled from "@/DefaultTheme";
+
+export const StyledGlobalPage = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+  width: "100%",
+  padding: "1rem",
+});
