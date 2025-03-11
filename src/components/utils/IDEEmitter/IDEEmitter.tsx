ae8d9060a18928920d469f413b5e97ed786576ef
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-03-11 17:35:40 +0200

    Created and used IDEEmitter

---
+++ src/components/utils/IDEEmitter/IDEEmitter.tsx
@@ -0,0 +1,33 @@
+import styled from "@/DefaultTheme";
+import { ReactNode } from "react";
+
+interface IDEEmitterProps {
+  code: string | ReactNode;
+  result?: string;
+}
+
+export const IDEEmitter = ({ code, result }: IDEEmitterProps) => (
+  <Root>
+    <CodeContainer>
+      <pre>
+        <code>{code}</code>
+      </pre>
+    </CodeContainer>
+    <div>{result}</div>
+  </Root>
+);
+
+const Root = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  border: `1px solid ${theme.palette.divider}`,
+  gap: "1rem",
+}));
+
+const CodeContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  padding: "1rem",
+  borderRadius: "4px",
+  backgroundColor: theme.palette.divider,
+}));
