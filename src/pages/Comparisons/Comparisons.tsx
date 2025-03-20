d3448d5a8987957746e69ec37763bb2bf75f50d9
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-03-20 17:24:17 +0200

    Removed unnecessary Comparisons styles

--- src/pages/Comparisons/Comparisons.tsx
+++ src/pages/Comparisons/Comparisons.tsx
@@ -6,10 +6,8 @@ export const Comparisons = () => (
   <StyledGlobalPage>
     <div>
       <Typography variant="h3">Comparisons</Typography>
-      <Typography>
-        We know many comparison operators from maths. In JavaScript they are
-        written like this:
-      </Typography>
+      We know many comparison operators from maths. In JavaScript they are
+      written like this:
       <ul>
         <li>
           <Highlight>==</Highlight> - equal to
@@ -36,17 +34,13 @@ export const Comparisons = () => (
           <Highlight>&lt;=</Highlight> - less than or equal to
         </li>
       </ul>
-      <Typography>
-        In this article we’ll learn more about different types of comparisons,
-        how JavaScript makes them, including important peculiarities. At the end
-        you’ll find a good recipe to avoid “JavaScript quirks”-related issues.
-      </Typography>
+      In this article we’ll learn more about different types of comparisons, how
+      JavaScript makes them, including important peculiarities. At the end
+      you’ll find a good recipe to avoid “JavaScript quirks”-related issues.
     </div>
     <div>
-      <Typography variant="h4">Boolean is the result</Typography>
-      <Typography>
-        A comparison result is always a boolean. For instance:
-      </Typography>
+      <Typography variant="h4">Boolean is the result</Typography>A comparison
+      result is always a boolean. For instance:
       <ul>
         <li>
           <Highlight>true</Highlight> – means “yes”, “correct” or “the truth”.
@@ -56,18 +50,14 @@ export const Comparisons = () => (
         </li>
       </ul>
       <IDEEmitter code="" />
-      <Typography>
-        A comparison result can be assigned to a variable, just like any value:
-      </Typography>
+      A comparison result can be assigned to a variable, just like any value:
       <IDEEmitter code="" />
     </div>
     <div>
       <Typography variant="h4">String comparison</Typography>
-      <Typography>
-        To see whether a string is greater than another, JavaScript uses the
-        so-called “dictionary” or “lexicographical” order. In other words,
-        strings are compared letter-by-letter.
-      </Typography>
+      To see whether a string is greater than another, JavaScript uses the
+      so-called “dictionary” or “lexicographical” order. In other words, strings
+      are compared letter-by-letter.
       <IDEEmitter code="" />
       <Typography>The algorithm to compare two strings is simple:</Typography>
       <ol>
@@ -87,13 +77,11 @@ export const Comparisons = () => (
           Otherwise, the longer string is greater.
         </li>
       </ol>
-      <Typography>
-        In the first example above, the comparison{" "}
-        <Highlight>'Z' {">"} 'A'</Highlight> gets to a result at the first step.
-        The second comparison <Highlight> 'Glow'</Highlight> and{" "}
-        <Highlight>'Glee'</Highlight> needs more steps as strings are compared
-        character-by-character:
-      </Typography>
+      In the first example above, the comparison{" "}
+      <Highlight>'Z' {">"} 'A'</Highlight> gets to a result at the first step.
+      The second comparison <Highlight> 'Glow'</Highlight> and{" "}
+      <Highlight>'Glee'</Highlight> needs more steps as strings are compared
+      character-by-character:
       <ol>
         <li>
           <Highlight>'G'</Highlight> is the same as <Highlight>'G'</Highlight>.
@@ -106,8 +94,6 @@ export const Comparisons = () => (
           Done!
         </li>
       </ol>
-    </div>
-    <div>
       <InfoBox
         title="Not a real dictionary, but Unicode order"
         content="The comparison algorithm given above is roughly equivalent to the one used in dictionaries or phone books, but it’s not exactly the same. For instance, case matters. A capital letter 'A' is not equal to the lowercase 'a'. Which one is greater? The lowercase 'a'. Why? Because the lowercase character has a greater index in the internal encoding table JavaScript uses (Unicode). We’ll get back to specific details and consequences of this in the chapter Strings."
@@ -115,30 +101,18 @@ export const Comparisons = () => (
     </div>
     <div>
       <Typography variant="h4">Comparison of different types</Typography>
-      <Typography>
-        When comparing values of different types, JavaScript converts the values
-        to numbers.
-      </Typography>
+      When comparing values of different types, JavaScript converts the values
+      to numbers.
+      <IDEEmitter code="" />A value <Highlight>null</Highlight> becomes{" "}
+      <Highlight>0</Highlight>, while <Highlight>true</Highlight> becomes{" "}
+      <Highlight>1</Highlight>.
       <IDEEmitter code="" />
-      <Typography>
-        A value <Highlight>null</Highlight> becomes <Highlight>0</Highlight>,
-        while <Highlight>true</Highlight> becomes <Highlight>1</Highlight>.
-      </Typography>
+      For example, let’s compare a string <Highlight>'2'</Highlight> with a
+      number <Highlight>1</Highlight>:
       <IDEEmitter code="" />
-      <Typography>
-        For example, let’s compare a string <Highlight>'2'</Highlight> with a
-        number <Highlight>1</Highlight>:
-      </Typography>
-      <IDEEmitter code="" />
-      <Typography>
-        The string <Highlight>'2'</Highlight> becomes the number{" "}
-        <Highlight>2</Highlight> (conversion happens automatically here).
-      </Typography>
-      <Typography>
-        Now, we have two numbers, so the comparison is simple and obvious.
-      </Typography>
-    </div>
-    <div>
+      The string <Highlight>'2'</Highlight> becomes the number{" "}
+      <Highlight>2</Highlight> (conversion happens automatically here). Now, we
+      have two numbers, so the comparison is simple and obvious.
       <InfoBox
         title="A funny consequence"
         content={
@@ -165,68 +139,50 @@ export const Comparisons = () => (
       />
     </div>
     <div>
-      <Typography variant="h4">Strict equality</Typography>
-      <Typography>
-        A regular equality check <Highlight>==</Highlight> has a problem. It
-        cannot differentiate 0 from <Highlight>false</Highlight>:
-      </Typography>
+      <Typography variant="h4">Strict equality</Typography>A regular equality
+      check <Highlight>==</Highlight> has a problem. It cannot differentiate 0
+      from <Highlight>false</Highlight>:
       <IDEEmitter code="" />
-      <Typography>
-        That’s because <Highlight>false</Highlight> is equivalent to{" "}
-        <Highlight>0</Highlight> due to automatic type conversion. For
-        comparisons like <Highlight>0 == false</Highlight> JavaScript converts{" "}
-        <Highlight>false</Highlight> to <Highlight>0</Highlight>.
-      </Typography>
-      <Typography>
-        The strict equality operator <Highlight>===</Highlight> checks the
-        equality without type conversion.
-      </Typography>
+      That’s because <Highlight>false</Highlight> is equivalent to{" "}
+      <Highlight>0</Highlight> due to automatic type conversion. For comparisons
+      like <Highlight>0 == false</Highlight> JavaScript converts{" "}
+      <Highlight>false</Highlight> to <Highlight>0</Highlight>. The strict
+      equality operator <Highlight>===</Highlight> checks the equality without
+      type conversion.
       <IDEEmitter code="" />
-      <Typography>
-        In this case, the result is <Highlight>false</Highlight> because the
-        types are different.
-      </Typography>
+      In this case, the result is <Highlight>false</Highlight> because the types
+      are different.
     </div>
     <div>
       <Typography variant="h4">Comparison with null and undefined</Typography>
-      <Typography>
-        An incomparable <Highlight>null</Highlight>?
-      </Typography>
+      An incomparable <Highlight>null</Highlight>?
       <IDEEmitter code="" />
-      <Typography>
-        The equality check <Highlight>==</Highlight> for{" "}
-        <Highlight>null</Highlight> and <Highlight>undefined</Highlight> is
-        defined such that, without any conversions, they equal each other and
-        don’t equal anything else.
-      </Typography>
+      The equality check <Highlight>==</Highlight> for{" "}
+      <Highlight>null</Highlight> and <Highlight>undefined</Highlight> is
+      defined such that, without any conversions, they equal each other and
+      don’t equal anything else.
       <IDEEmitter code="" />
-      <Typography>
-        For strict equality check <Highlight>===</Highlight> there is no
-        conversion. So <Highlight>null === undefined</Highlight> only if they
-        are of the same type.
-      </Typography>
+      For strict equality check <Highlight>===</Highlight> there is no
+      conversion. So <Highlight>null === undefined</Highlight> only if they are
+      of the same type.
       <Typography variant="h4">Strange result: null vs 0</Typography>
-      <Typography>
-        Let’s compare <Highlight>null</Highlight> with a zero:
-      </Typography>
+      Let’s compare <Highlight>null</Highlight> with a zero:
       <IDEEmitter code="" />
-      <Typography>
-        Mathematically, that’s strange. The last result states that “
-        <Highlight>null</Highlight> is greater than or equal to zero”, so in one
-        of the comparisons above it must be <Highlight>true</Highlight>, but
-        they are both false. The reason is that an equality check{" "}
-        <Highlight>==</Highlight> and comparisons{" "}
-        <Highlight>{"> < >= <="}</Highlight> work differently. Comparisons
-        convert <Highlight>null</Highlight> to a number, treating it as{" "}
-        <Highlight>0</Highlight>. That’s why (3){" "}
-        <Highlight>null {">="} 0</Highlight> is true and (1){" "}
-        <Highlight>null {">"} 0</Highlight> is false. On the other hand, the
-        equality check <Highlight>==</Highlight> for{" "}
-        <Highlight>undefined</Highlight> and <Highlight>null</Highlight> is
-        defined such that, without any conversions, they equal each other and
-        don’t equal anything else. That’s why (2){" "}
-        <Highlight>null == 0</Highlight> is false.
-      </Typography>
+      Mathematically, that’s strange. The last result states that “
+      <Highlight>null</Highlight> is greater than or equal to zero”, so in one
+      of the comparisons above it must be <Highlight>true</Highlight>, but they
+      are both false. The reason is that an equality check{" "}
+      <Highlight>==</Highlight> and comparisons{" "}
+      <Highlight>{"> < >= <="}</Highlight> work differently. Comparisons convert{" "}
+      <Highlight>null</Highlight> to a number, treating it as{" "}
+      <Highlight>0</Highlight>. That’s why (3){" "}
+      <Highlight>null {">="} 0</Highlight> is true and (1){" "}
+      <Highlight>null {">"} 0</Highlight> is false. On the other hand, the
+      equality check <Highlight>==</Highlight> for{" "}
+      <Highlight>undefined</Highlight> and <Highlight>null</Highlight> is
+      defined such that, without any conversions, they equal each other and
+      don’t equal anything else. That’s why (2) <Highlight>null == 0</Highlight>{" "}
+      is false.
     </div>
     <div>
       <Typography variant="h4">An incomparable undefined</Typography>
